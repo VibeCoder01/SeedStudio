3e9e536bab024218e8e825c78062b5690fc2ca09
Shaun Dunmall <sdunmall@gmail.com>
2025-11-03 23:49:43 +0000

    npm test
    > nextn@0.1.0 test
    > jest
    
     PASS  src/__tests__/PageHeader.tes

--- jest.config.ts
+++ jest.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   preset: 'ts-jest',
-  transformIgnorePatterns: ['/node_modules/(?!lucide-react).+\\.js$'],
+  transformIgnorePatterns: ['/node_modules/(?!lucide-react).+\\.(js|jsx|ts|tsx)$'],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
